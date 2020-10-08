29d2d75785aaa213fdf5628a1e1c0e02076b83be
Farhad Tavakoli <farhad.tavakoli81@yahoo.com>
2020-10-08 22:05:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
